4666a0af87c02121e4015f853ae3908cd9725cbc
xcv58 <i@xcv58.com>
2019-04-24 23:06:25 -0700

    Add screenshots

---
+++ test_driver/app_test.dart
@@ -0,0 +1,36 @@
+// Imports the Flutter Driver API
+import 'package:flutter_driver/flutter_driver.dart';
+import 'package:test/test.dart';
+import 'package:screenshots/config.dart';
+import 'package:screenshots/capture_screen.dart';
+
+void main() {
+  final Map config = Config().config;
+
+  group('Counter App', () {
+    // First, define the Finders. We can use these to locate Widgets from the
+    // test suite. Note: the Strings provided to the `byValueKey` method must
+    // be the same as the Strings we used for the Keys in step 1.
+    final counterTextFinder = find.byValueKey('counter');
+    final buttonFinder = find.byValueKey('increment');
+
+    FlutterDriver driver;
+
+
+    // Connect to the Flutter driver before running any tests
+    setUpAll(() async {
+      driver = await FlutterDriver.connect();
+    });
+
+    // Close the connection to the driver after the tests have completed
+    tearDownAll(() async {
+      if (driver != null) {
+        driver.close();
+      }
+    });
+
+    test('screenshot', () async {
+      await screenshot(driver, config, 'main_app');
+    });
+  });
+}
